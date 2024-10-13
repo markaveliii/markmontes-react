ed2c278b0f667be1a5228cb8ab7e0aa098a70e05
Mark Montes <mark.montes@lumencor.com>
2024-10-12 17:33:05 -0700

    idk wassup wit these assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,14 @@ export default {
         sans: ['Verdana' , 'sans-serif']
       },
       backgroundImage:{
-        'space' : "url('assets/nasa-unsplash.jpg')",
-        'goldLight' : "url('assets/rene-bohmer.jpg')",
-        'bwBox' : "url('assets/shubham-dhage.jpg')",
-        'goldRain' : "url('assets/joshua-sortino.jpg')",
-        'blueRain' : "url('assets/christopher-burns.jpg')",
-        'blackRain' : "url('assets/blackCubeRain.jpg')",
-        'goldForest' : "url('assets/goldForest.jpg')",
-        'meteorShower' : "url('assets/meteorShower.jpg')",
+        'space' : "url('/assets/nasa-unsplash.jpg')",
+        'goldLight' : "url('/assets/rene-bohmer.jpg')",
+        'bwBox' : "url('/assets/shubham-dhage.jpg')",
+        'goldRain' : "url('/assets/joshua-sortino.jpg')",
+        'blueRain' : "url('/assets/christopher-burns.jpg')",
+        'blackRain' : "url('/assets/blackCubeRain.jpg')",
+        'goldForest' : "url('/assets/goldForest.jpg')",
+        'meteorShower' : "url('/assets/meteorShower.jpg')",
       },
       gridTemplateColumns: {
         '70/30' : '70% 28%',

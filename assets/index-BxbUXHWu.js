164c3da716037034b5f378c44d450a33bcf207cd
markaveliii <markaveliii@users.noreply.github.com>
2024-10-20 23:49:33 +0000

    deploy: a67f1a816c026ab427da04bb7e7a3ca52f417dcc

--- assets/index-BxbUXHWu.js
+++ assets/index-BxbUXHWu.js
@@ -59,7 +59,7 @@ Error generating stack: `+i.message+`
             justify-center
             h-3/4
             w-2/4
-            '`,children:N.jsx("p",{className:"text-white p-20",children:e})})})}function Wl({bannerColor:e,bannerBG:n,bgImage:t,bgColor:r,title:l,text:i}){return N.jsxs("div",{className:"snap-y snap-mandatory h-screen scroll-smooth w-screen",children:[N.jsx(jd,{bgImage:n,title:l,bgColor:r}),N.jsx("div",{className:`
+            '`,children:N.jsx("p",{className:"text-white p-20",children:e})})})}function Wl({bannerColor:e,bannerBG:n,bgImage:t,bgColor:r,title:l,text:i}){return N.jsxs("div",{className:"snap-y h-screen scroll-smooth w-screen",children:[N.jsx(jd,{bgImage:n,title:l,bgColor:r}),N.jsx("div",{className:`
                     ${t}
                     '
                     bg-cover
